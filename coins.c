d7db79923133a8bfb825b06368668455d780303f
Nandni1995 <nandnis95@gmail.com>
2016-07-05 10:00:50 +0530

    Create coins.c

---
+++ coins.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j;
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+if(n<3)
+{
+printf("%d",n);
+}
+if(n<5 && n>=3)
+{
+i=n%3+n/3;
+printf("%d",i);
+}
+if(n>=5)
+{
+j=n%5+n/5;
+printf("%d",j);
+}
+getch();
+}
